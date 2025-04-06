8e9c6fdbc08703c8db8cf624c3e5cd7db716210b
duanshi-26 <duanshichawla2612@gmail.com>
2025-04-06 22:25:30 +0530

    commit

---
+++ Day_1/01basicreact/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+   <h1>Duanshi JPMC's first create react-app project</h1>
+  );
+}
+
+export default App;
---
+++ Day_1/01vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1> Duanshi JPMC's first vite react project </h1>
+    </>
+  )
+}
+
+export default App
